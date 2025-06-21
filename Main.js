9f3bb115bd5de941efd46f23a4539f5cf3e253a7
Tommy Azwar Muslim <azwarmuslimtommy@gmail.com>
2025-06-21 18:37:44 +0700

    initial commit

---
+++ Main.js
@@ -0,0 +1,15 @@
+document.getElementById("Submit").onclick = function (){
+var tebakan = document.getElementById("tebakan").value;
+	
+	var password = "jari-jari";
+	
+	if (tebakan == password)
+	{
+		alert("Password Anda Benar!")
+	}
+	else{
+		alert("Password Anda Salah!")
+	}
+	}
+		
+			
